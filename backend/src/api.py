6f2a2b4838382c2f570dbfd2e2573fae9679d622
Wei Jie Lam <weijielam@gmail.com>
2021-01-13 18:54:08 +0000

    fixed error with typed-ast dependency by upgrading it in pip, updated requirements.txt file

--- backend/src/api.py
+++ backend/src/api.py
@@ -16,7 +16,7 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+#db_drop_and_create_all()
 
 ## ROUTES
 '''
@@ -40,12 +40,13 @@ def get_drinks():
 '''
 @TODO implement endpoint
     GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
+        requires the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
 def get_drinks_detail():
     drinks = Drink.query.all()
 
@@ -57,23 +58,32 @@ def get_drinks_detail():
 '''
 @TODO implement endpoint
     POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
+        create a new row in the drinks table
+        requires the 'post:drinks' permission
+        contains the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks', methods['POST'])
-# TODO: add auth req
-def create_drink(payload):
-    try:
-        print('placeholder')
-    except:
-        print('placeholder exception')
-    return jsonify({
-        'success': True,
-        'drinks': 'placeholder' # TODO: replace with drink details
-    }), 200
+# @app.route('/drinks', methods['POST'])
+# @requires_auth('post:drinks') 
+# def create_drink(payload):
+#     try:
+#         # load data from request
+#         data = request.get_json()
+
+#         # create new Drink         
+#         drink = Drink()
+#         drink.title = data['title']
+#         drink.recipe = json.dumps(data['recipe'])
+#         drink.insert()
+
+#     except:
+#         abort(400)
+
+#     return jsonify({
+#         'success': True,
+#         'drinks': 'placeholder' # TODO: replace with drink details
+#     }), 200
 
 '''
 @TODO implement endpoint
@@ -87,16 +97,20 @@ def create_drink(payload):
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
 # TODO: add auth req
 def update_drink(id):
-    data = request.get_json()
-    drink = Drink.query.filter_by(Drink.id==id).one_or_none()
-    
-    if (drink is None):
-        abort(404)
     try:
+        data = request.get_json()
+        drink = Drink.query.filter_by(Drink.id==id).one_or_none()
+        
+        # data validation
+        if (drink is None):
+            abort(404)
+        
         print('placeholder')
     except:
+        abort(400)
         print('placeholder')
 
 

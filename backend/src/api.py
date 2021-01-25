6588e07b5688d3df7f94d5a449493ef5dc9290ee
Wei Jie Lam <weijielam@gmail.com>
2021-01-25 20:53:21 +0000

    Issue with project, starting new dev branch, setting up virtual environment and dependencies

--- backend/src/api.py
+++ backend/src/api.py
@@ -16,7 +16,7 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-db_drop_and_create_all()
+# db_drop_and_create_all()
 
 ## ROUTES
 '''
@@ -27,63 +27,28 @@ db_drop_and_create_all()
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks', methods=['GET'])
-def get_drinks():
-    drinks = Drink.query.all()
-
-    return jsonify({
-        'success': True,
-        'drinks': [drink.short() for drink in drinks]
-    }), 200
 
 
 '''
 @TODO implement endpoint
     GET /drinks-detail
-        requires the 'get:drinks-detail' permission
+        it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks-detail', methods=['GET'])
-# @requires_auth('get:drinks-detail')
-def get_drinks_detail():
-    drinks = Drink.query.all()
 
-    return jsonify({
-        'success': True,
-        'drinks': [drink.long() for drink in drinks]
-    }), 200
 
 '''
 @TODO implement endpoint
     POST /drinks
-        create a new row in the drinks table
-        requires the 'post:drinks' permission
-        contains the drink.long() data representation
+        it should create a new row in the drinks table
+        it should require the 'post:drinks' permission
+        it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-# @app.route('/drinks', methods['POST'])
-# @requires_auth('post:drinks') 
-# def create_drink(payload):
-#     try:
-#         # load data from request
-#         data = request.get_json()
 
-#         # create new Drink         
-#         drink = Drink()
-#         drink.title = data['title']
-#         drink.recipe = json.dumps(data['recipe'])
-#         drink.insert()
-
-#     except:
-#         abort(400)
-
-#     return jsonify({
-#         'success': True,
-#         'drinks': 'placeholder' # TODO: replace with drink details
-#     }), 200
 
 '''
 @TODO implement endpoint
@@ -96,22 +61,6 @@ def get_drinks_detail():
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks/<int:id>', methods=['PATCH'])
-@requires_auth('patch:drinks')
-# TODO: add auth req
-def update_drink(id):
-    try:
-        data = request.get_json()
-        drink = Drink.query.filter_by(Drink.id==id).one_or_none()
-        
-        # data validation
-        if (drink is None):
-            abort(404)
-        
-        print('placeholder')
-    except:
-        abort(400)
-        print('placeholder')
 
 
 '''
@@ -124,16 +73,20 @@ def update_drink(id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
-@app.route('/drinks/<int:id>', methods=['DELETE'])
-# TODO: add auth req
-def delete_drink(id):
-    try:
-        print('placeholder')
-    except:
-        print('placeholder')
 
 
 ## Error Handling
+'''
+Example error handling for unprocessable entity
+'''
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+                    "success": False, 
+                    "error": 422,
+                    "message": "unprocessable"
+                    }), 422
+
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
@@ -145,34 +98,10 @@ def delete_drink(id):
 
 '''
 
-
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above 
 '''
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({
-        'success': False,
-        'error': 404,
-        'message': 'Resource not found.'
-    }), 404
-
-@app.errorhandler(422)
-def unprocessable(error):
-    return jsonify({
-        'success': False,
-        'error': 422, 
-        'message': 'Unprocessable entity.'
-    }), 422
-
-@app.errorhandler(500)
-def internal_server(error):
-    return jsonify({
-        'success': False,
-        'error': 500, 
-        'message': 'Internal server error. Please try again later.'
-    }), 500
 
 
 '''
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,12 +3,9 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-# database_filename = "database.db"
-# project_dir = os.path.dirname(os.path.abspath(__file__))
-# database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
-
-database_name = 'coffee_shop'
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
